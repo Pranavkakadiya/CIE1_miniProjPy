3dccac4de03ebc5c33722155e2c1de3bf39030e9
Pranav <pkakadiya274@rku.ac.in>
2020-08-22 19:55:41 +0530

    complete project of database

---
+++ Project/PermanentEmployee.py
@@ -0,0 +1,12 @@
+class Per_Emp:
+
+      basic_salary = 5000
+      def calculatesalary(self,exp):
+            if (exp >= 15):
+                  return self.basic_salary + (self.basic_salary * 20) / 100
+            elif (10 <= exp < 15):
+                  return self.basic_salary + (self.basic_salary * 10) / 100
+            elif (5 <= exp < 10):
+                  return self.basic_salary + (self.basic_salary * 5) / 100
+            else:
+                  return self.basic_salary
